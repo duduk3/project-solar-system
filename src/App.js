cf08c9e85df226f4c295519b41bc9d45f3d00767
EDUARDO FERREIRA <dupearljam@gmail.com>
2022-02-18 17:30:22 -0300

    adiciona componentes e renderiza ate req=>6

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
-    return (<h1>Sistema Solar</h1>);
+    return (
+      <>
+        <Header />
+        <SolarSystem />
+      </>
+    );
   }
 }
 
